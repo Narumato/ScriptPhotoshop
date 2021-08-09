e0cf99a8d6271db49093105465f34ae3b13664a4
Sebastien Belanger <sebastien.belan@gmail.com>
2021-08-09 08:46:15 -0400

    Added opening html file to verify at the end
    Added opening html file to verify at the end

--- SuperExportsebbass.jsx
+++ SuperExportsebbass.jsx
@@ -1,12 +1,58 @@
 var docRef = app.activeDocument;
 
+// Create and setup folder to save image
+var desktopDir = "/Users/sebbelg/Desktop/DoneEmieCadre";
+var folderName = "/image0101010222";
+var currentFolder = Folder(desktopDir + folderName);
+//Check if it exist, if not create it.
+if(!currentFolder.exists) currentFolder.create();
+
 
 var savedState = app.activeDocument.activeHistoryState
 
-//var bounds = [0, 0, 250, 250];
-//app.activeDocument.crop(bounds);
+
+// Sauvegarde de l'image de coté
+var bounds = [0, 0, 50, 50];
+app.activeDocument.crop(bounds);
+
+var fileName = "/contour D.png";
+var fileNamePath = desktopDir + folderName + fileName;
+var pngFile = new File(fileNamePath);
+
+pngSaveOptions = new PNGSaveOptions();
+pngSaveOptions.compression = 0;
+pngSaveOptions.interlaced = false;
+
+activeDocument.saveAs(pngFile, pngSaveOptions, true, Extension.LOWERCASE);
+
+app.activeDocument.activeHistoryState = savedState
 
 
+// Sauvegarde de l'image du coin
+bounds = [0, 50, 50, 100];
+app.activeDocument.crop(bounds);
+
+fileName = "/coin BD.png";
+fileNamePath = desktopDir + folderName + fileName;
+pngFile = new File(fileNamePath);
+
+pngSaveOptions = new PNGSaveOptions();
+pngSaveOptions.compression = 0;
+pngSaveOptions.interlaced = false;
+
+activeDocument.saveAs(pngFile, pngSaveOptions, true, Extension.LOWERCASE);
+
+app.activeDocument.activeHistoryState = savedState
+
+function help() {
+	try{
+		var URL = new File("file:///Users/sebbelg/Desktop/DoneEmieCadre/image0101010222/Test.html");
+		URL.execute();
+	}catch(e){
+		alert("Error, Can Not Open.");
+	};
+}
+help();
 
 // var dlg = new Window('dialog', 'My first script!',[100,100,480,250]);
 // dlg.btnPnl = dlg.add('panel', [25,15,365,125], 'Hello world!');
@@ -17,24 +63,3 @@ var savedState = app.activeDocument.activeHistoryState
 //    alert("Congratulations - it all worked!");
 //    dlg.close();
 // }
-
-//activeDocument.saveAs("contour D_testScript");
-
-
-var desktopDir = "/Users/sebbelg/Desktop/DoneEmieCadre";
-var folderName = "/image0101010";
-var currentFolder = Folder(desktopDir + folderName);
-//Check if it exist, if not create it.
-if(!currentFolder.exists) currentFolder.create();
-var fileName = "/contourDroit.png";
-var path22 = desktopDir + folderName + fileName;
-alert(path22);
-var pngFile = new File(path22);
-
-pngSaveOptions = new PNGSaveOptions();
-pngSaveOptions.compression = 0;
-pngSaveOptions.interlaced = false;
-
-activeDocument.saveAs(pngFile, pngSaveOptions, true, Extension.LOWERCASE);
-
-app.activeDocument.activeHistoryState = savedState
